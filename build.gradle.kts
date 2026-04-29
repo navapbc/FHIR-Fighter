6e5d35f7d38a534ded97c425e967297719c7d716
Josh Fisk <joshuafisk@navapbc.com>
2026-04-29 13:07:26 -0700

    Setting version for 2.0.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ publishing {
         create<MavenPublication>("prime-fhirfighter") {
             groupId = "com.nava.prime"
             artifactId = "fhir-fighter"
-            version = "1.0.0-SNAPSHOT"
+            version = "2.0.0"
 
             from(components["java"])
         }
